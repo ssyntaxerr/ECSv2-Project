4b960d74bc973ef0d10f842fc4a7c70ba572e324
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-23 14:46:58 +0100

    Update root module variables

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -2,3 +2,24 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-2"
 }
+
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  default     = [
+    "eu-west-2a",
+    "eu-west-2b"
+  ]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  default     = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+}
