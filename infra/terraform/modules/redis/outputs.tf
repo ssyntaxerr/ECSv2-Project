8edfd5b3e17eebc53479eb5dff68a2ad672f5bb6
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-25 15:22:24 +0100

    Add Redis module outputs

---
+++ infra/terraform/modules/redis/outputs.tf
@@ -0,0 +1,11 @@
+output "redis_endpoint" {
+  value = aws_elasticache_replication_group.redis.primary_endpoint_address
+}
+
+output "redis_port" {
+  value = aws_elasticache_replication_group.redis.port
+}
+
+output "security_group_id" {
+  value = aws_security_group.redis.id
+}
