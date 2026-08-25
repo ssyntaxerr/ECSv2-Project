9e6ec0661d63e154937b1f44ee7a62193297efe1
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-25 18:52:19 +0100

    Update Redis module outputs + ECS module

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -41,4 +41,7 @@ module "ecs" {
 
   name_prefix = local.name_prefix
   common_tags = local.common_tags
+
+  vpc_id = module.vpc.vpc_id
+  vpc_cidr = var.vpc_cidr
 }
--- infra/terraform/modules/redis/outputs.tf
+++ infra/terraform/modules/redis/outputs.tf
@@ -6,6 +6,6 @@ output "redis_port" {
   value = aws_elasticache_replication_group.redis.port
 }
 
-output "security_group_id" {
-  value = aws_security_group.redis.id
+output "redis_security_group_id" {
+  value = aws_security_group.redis_sg.id
 }
