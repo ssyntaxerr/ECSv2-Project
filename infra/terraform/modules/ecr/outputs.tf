40e1d102a31f2db9cd1d51d445ba10db526759fb
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-25 11:52:38 +0100

    Add ECR module outputs

---
+++ infra/terraform/modules/ecr/outputs.tf
@@ -0,0 +1,13 @@
+output "repo_urls" {
+    value = {
+        for name, repository in aws_ecr_repository.ecsv2-repos :
+        name => repository.repository_url
+    }
+}
+
+output "repo_arns" {
+    value = {
+        for name, repository in aws_ecr_repository.ecsv2-repos :
+        name => repository.arn
+    }  
+}
