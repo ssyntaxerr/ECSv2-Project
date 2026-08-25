c98c16e7929be7aa252ba470bcacfb4578dee73c
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-25 22:51:49 +0100

    Update VPC outputs + module

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -8,6 +8,7 @@ module "vpc" {
   vpc_cidr = var.vpc_cidr
   availability_zones = var.availability_zones
   private_subnet_cidrs = var.private_subnet_cidrs
+  public_subnet_cidrs = var.public_subnet_cidrs
 }
 
 module "ecr" {
--- infra/terraform/modules/vpc/outputs.tf
+++ infra/terraform/modules/vpc/outputs.tf
@@ -5,3 +5,7 @@ output "vpc_id" {
 output "private_subnet_ids" {
     value = [for subnet in aws_subnet.priv-subnet : subnet.id]
 }
+
+output "public_subnet_ids" {
+  value = [for subnet in aws_subnet.pub-subnet : subnet.id]
+}
