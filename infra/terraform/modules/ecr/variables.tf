65c8409ec3bc60af4ec712fba23dad78bd73cd5e
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-25 11:43:14 +0100

    Create ECR module + variables

---
+++ infra/terraform/modules/ecr/variables.tf
@@ -0,0 +1,17 @@
+variable "name_prefix" {
+  type        = string
+}
+
+variable "common_tags" {
+  type        = map(string)
+}
+
+variable "repositories" {
+  type        = list(string)
+
+  default = [
+    "api-repo",
+    "worker-repo",
+    "dashboard-repo"
+  ]
+}
