3cc1b649e6488948f8b5db27b65688ca191d05d4
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-25 14:29:16 +0100

    Create RDS outputs

---
+++ infra/terraform/modules/rds/outputs.tf
@@ -0,0 +1,20 @@
+output "db_endpoint" {
+    value = aws_db_instance.postgres.address
+}
+
+output "db_port" {
+  value = aws_db_instance.postgres.port
+}
+
+output "db_name" {
+  value = aws_db_instance.postgres.name
+}
+
+output "db_username" {
+  value = aws_db_instance.postgres.username
+  sensitive = true
+}
+
+output "db_sg_id" {
+  value = aws_security_group.postgres_sg.id
+}
