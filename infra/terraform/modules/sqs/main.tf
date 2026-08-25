d853b5c68d7b8821b5e384d479d75bc8126bfda8
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-25 13:03:34 +0100

    Add SQS DLQ and attach to analytics queue with redrive policy

--- infra/terraform/modules/sqs/main.tf
+++ infra/terraform/modules/sqs/main.tf
@@ -9,5 +9,24 @@ resource "aws_sqs_queue" "analytics" {
         Name = "${var.name_prefix}-analytics"
         Service = "worker"
     })
-  
+}
+
+resource "aws_sqs_queue" "analytics_dlq" {
+    name = "${var.name_prefix}-analytics-dlq"
+
+    message_retention_seconds = 1209600
+
+    tags = merge(var.common_tags, {
+        Name = "${var.name_prefix}-analytics-dlq"
+        Service = "worker"
+    })
+}
+
+resource "aws_sqs_queue_redrive_policy" "analytics_redrive" {
+    queue_url = aws_sqs_queue.analytics.id
+    
+    redrive_policy = jsonencode({
+        deadLetterTargetArn = aws_sqs.analytics_dlq.arn
+        maxReceiveCount = 3
+    })
 }
