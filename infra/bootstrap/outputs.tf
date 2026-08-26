41340a9cb699a3b3bbea09401e76cdca7695ffd2
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-26 13:22:20 +0100

    Add Github Actions role arn to outputs

--- infra/bootstrap/outputs.tf
+++ infra/bootstrap/outputs.tf
@@ -5,3 +5,7 @@ output "state_bucket_name" {
 output "acm_certificate_arn" {
   value = aws_acm_certificate_validation.main.certificate_arn
 }
+
+output "github_actions_role_arn" {
+  value       = aws_iam_role.github_actions.arn
+}
