f3e8ada0543690a830e6a406a71eb8a336e2104a
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-26 15:34:55 +0100

    Add ECS sg to ECS_Security module

---
+++ infra/terraform/modules/ecs_security/main.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "ecs_sg" {
+  name        = "${var.name_prefix}-ecs-sg"
+  description = "Security group for ECS tasks"
+  vpc_id      = var.vpc_id
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "API traffic from ALB"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    security_groups = [var.alb_security_group_id]
+  }
+
+  ingress {
+    description = "Dashboard traffic from ALB"
+    from_port = 8081
+    to_port = 8081
+    protocol = "tcp"
+    security_groups = [var.alb_security_group_id]
+  }
+
+  tags = merge(var.common_tags, {
+    Name    = "${var.name_prefix}-ecs-sg"
+    Service = "ecs"
+  })
+}
