c66d0a90238db375c594d4fab64dba4eb855cbb0
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-26 14:00:18 +0100

    Modify ALB sg to accept HTTPS

--- infra/terraform/modules/alb/main.tf
+++ infra/terraform/modules/alb/main.tf
@@ -2,7 +2,7 @@ resource "aws_security_group" "alb_sg" {
   name = "${var.name_prefix}-alb-sg"
   vpc_id = var.vpc_id
 
-  ingress = {
+  ingress {
     description = "HTTP from internet"
     from_port = 80
     to_port = 80
@@ -10,7 +10,15 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress = {
+  ingress {
+    description = "HTTPS from internet"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     description = "Allow outbound traffic"
     from_port = 0
     to_port = 0 
