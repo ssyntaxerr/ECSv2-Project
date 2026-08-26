0c9de594b76e4c7651ef2ca0a886da3bdb1b6e22
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-26 16:09:01 +0100

    Wire Redis module to root config

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -59,3 +59,14 @@ module "rds" {
   db_username = var.db_username
   db_password = var.db_password
 }
+
+module "redis" {
+  source = "./modules/redis"
+
+  name_prefix = local.name_prefix
+  common_tags = local.common_tags
+
+  vpc_id = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+  ecs_security_group_id = module.ecs_security.ecs_security_group_id
+}
