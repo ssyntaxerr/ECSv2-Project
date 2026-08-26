29d5ff2c1db190d05e7723c0f1b900d02edb728c
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-26 15:49:17 +0100

    Wire ECR & SQS modules to root config

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -9,3 +9,18 @@ module "vpc" {
   private_subnet_cidrs = var.private_subnet_cidrs
   public_subnet_cidrs = var.public_subnet_cidrs
 }
+
+module "ecr" {
+  source = "./modules/ecr"
+
+  name_prefix = local.name_prefix
+  common_tags = local.common_tags
+  repositories = var.repositories
+}
+
+module "sqs" {
+  source = "./modules/sqs"
+
+  name_prefix = local.name_prefix
+  common_tags = local.common_tags
+}
