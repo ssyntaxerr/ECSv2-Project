51c5f0bbfdb6e73be486af32754ae3fbb59e4ed3
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-26 11:52:24 +0100

    Create ECS Worker service

--- infra/terraform/modules/ecs/main.tf
+++ infra/terraform/modules/ecs/main.tf
@@ -268,3 +268,31 @@ resource "aws_ecs_service" "api" {
     Service = "api"
   })
 }
+
+resource "aws_ecs_service" "worker" {
+  name = "${var.name_prefix}-worker-service"
+  cluster = aws_ecs_cluster.ecsv2_cluster.id
+  task_definition = aws_ecs_task_definition.worker.arn
+
+  desired_count = 1
+  launch_type = "FARGATE"
+
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent = 200
+
+  deployment_circuit_breaker {
+    enable = true
+    rollback = true
+  }
+
+  network_configuration {
+    subnets = var.private_subnet_ids
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = false
+  }
+
+  tags = merge(var.common_tags, {
+    Name = "${var.name_prefix}-worker-service"
+    Service = "worker"
+  })
+}
