c90d8be4d299c06d31a7e8bd75cdc6bf09176fe1
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-26 13:47:32 +0100

    Add rule to WAF policy + web_acl_arn output

--- infra/terraform/modules/waf/main.tf
+++ infra/terraform/modules/waf/main.tf
@@ -28,6 +28,28 @@ resource "aws_wafv2_web_acl" "main" {
     }
   }
 
+  rule {
+    name = "RateLimit"
+    priority = 2
+
+    action {
+      block{}
+    }
+
+    statement {
+      rate_based_statement {
+        limit = 2000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name = "${var.name_prefix}-rate-limit"
+      sampled_requests_enabled = true
+    }
+  }
+
   visibility_config {
     cloudwatch_metrics_enabled = true
     metric_name = "${var.name_prefix}-waf"
@@ -37,4 +59,9 @@ resource "aws_wafv2_web_acl" "main" {
   tags = merge(var.common_tags, {
     Name = "${var.name_prefix}-waf"
   })
+}
+
+resource "aws_wafv2_web_acl_association" "alb" {
+  resource_arn = var.alb_arn
+  web_acl_arn  = aws_wafv2_web_acl.main.arn
 }
