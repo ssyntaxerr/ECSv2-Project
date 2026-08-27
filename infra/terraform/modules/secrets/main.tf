6b5e695237d66fb53eaa116ea0a0cf5d7e43b157
ssyntaxer <ssyntaxerr@gmail.com>
2026-08-27 13:23:43 +0100

    Fix bugs

--- infra/terraform/modules/secrets/main.tf
+++ infra/terraform/modules/secrets/main.tf
@@ -11,6 +11,6 @@ resource "aws_secretsmanager_secret_version" "postgres_secrets" {
   secret_id = aws_secretsmanager_secret.postgres_secret.id
 
   secret_string = jsonencode({
-    DATABASE_URL = "postgresql://${var.db_username}:${var.db_password}@${var.db_endpoint}:${var.db_name}?sslmode=require"
+    DATABASE_URL = "postgresql://${var.db_username}:${var.db_password}@${var.db_endpoint}:${var.db_port}/${var.db_name}?sslmode=require"
   })
 }
